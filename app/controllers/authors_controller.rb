137a0f8398b08a5166249246ab12125d3e12c39c
Jack Madden <jck13mad@gmail.com>
2021-02-26 21:46:14 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,6 +4,12 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
   end
   
+  def post_index
+    @author = Author.find(params[:id])
+    @posts = @author.posts 
+    render template: 'posts/index'
+  end
+  
   def post 
     @author = Author.find(params[:id])
     @post = Post.find(params[:post_id])
