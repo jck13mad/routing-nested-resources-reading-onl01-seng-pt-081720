848558c11e76ace9893cf3176cf441987594351e
Jack Madden <jck13mad@gmail.com>
2021-02-26 21:40:48 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,5 +3,11 @@ class AuthorsController < ApplicationController
   def show
     @author = Author.find(params[:id])
   end
+  
+  def post 
+    @author = Author.find(params[:id])
+    @post = Post.find(params[:post_id])
+    render template: 'posts/show'
+  end
 
 end
